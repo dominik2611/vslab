6e6a05f3db588d08a9e0329da26029b8ee66d265
Dominik Bartos <dominikbartos@yahoo.de>
2019-11-05 14:16:39 +0100

    initial commit

---
+++ UserService/src/test/java/de/hska/UserService/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hska.UserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
