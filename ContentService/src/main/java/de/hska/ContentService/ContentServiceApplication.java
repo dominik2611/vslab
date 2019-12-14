d71b4e50910810ad7e53c4676b5ce64e5a61051f
Dominik Bartos <dominikbartos@yahoo.de>
2019-12-14 18:14:22 +0100

    added hystrix  to content service (single service for hystrix is not working)

--- ContentService/src/main/java/de/hska/ContentService/ContentServiceApplication.java
+++ ContentService/src/main/java/de/hska/ContentService/ContentServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 @RibbonClient("content-service")
 public class ContentServiceApplication {
 
